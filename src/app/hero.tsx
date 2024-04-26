ee1a07cddd8164f3d5f6439261d4878f1f97752b
Albert Shih <albertshih3@gmail.com>
2024-04-25 23:16:05 -0700

    New Landing Page + Navbar
    Updated landing page and added navigation bar to site.
    
    Signed-off-by: Albert Shih <albertshih3@gmail.com>

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,24 +1,20 @@
 import { Button, Spacer, Image } from "@nextui-org/react";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div className="flex items-center justify-around h-96">
-      <div>
-        <h1 style={{ fontSize: "40px", fontWeight: "bold" }}>
-          Hi there! My name is Albert.
+    <div className="flex flex-col md:flex-row items-center justify-around h-screen w-full">
+      <div className="text-center sm:text-left p-4">
+        <h1 className="text-3xl sm:text-6xl font-bold">
+          Hi there! <span className="text-blue-600">My name is Albert.</span>
         </h1>
-        <p className="mb-5" style={{ color: "#777" }}>
+        <p className="mt-2 mb-5 text-gray-500">
           I am a computer science student and aspiring full stack developer.
         </p>
-        <Button color="primary">Learn More</Button>
+        <Button color="primary">Get to know me!</Button>
       </div>
-      <div>
-        <Image
-          src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-          alt="Hero Image"
-          width={400}
-          height={400}
-        />
+      <div className="hidden md:flex w-1/4">
+        <Image src="headshot.jpeg" alt="Hero Image" width={400} height={400} />
       </div>
     </div>
   );
